56825711c4899d016ffe96e22b8ff463a5bf7f63
lei.zhang <1015994684@qq.com>
2018-08-16 20:27:27 +0800

    微调

--- config-client/src/main/java/com/example/configclient/web/ConfigController.java
+++ config-client/src/main/java/com/example/configclient/web/ConfigController.java
@@ -2,6 +2,7 @@ package com.example.configclient.web;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +10,7 @@ public class ConfigController {
     @Value("${userName}")
     private String userName;
     @GetMapping("/userName")
-    public String getUserName(){
+    public String getUserName(@RequestParam("name") String name){
         return userName;
     }
 }
---
+++ serice-feign/src/main/java/com/example/servicefeign/config/SwaggerConfig.java
@@ -0,0 +1,4 @@
+package com.example.servicefeign.config;
+
+public class SwaggerConfig {
+}

8591123f3abf4dbbcf70f8e4681af13a98009251
lei.zhang <1015994684@qq.com>
2018-08-17 20:26:47 +0800

    微调

--- config-client/src/main/java/com/example/configclient/main/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/main/ConfigClientApplication.java
@@ -1,10 +1,12 @@
 package com.example.configclient.main;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
@@ -16,5 +18,11 @@ public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
+    }
+    @Value("${userName}")
+    private String userName;
+    @GetMapping("/userName")
+    public String getUserName(){
+        return userName;
     }
 }
